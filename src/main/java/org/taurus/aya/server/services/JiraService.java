f9bc4b5821dd55f736b0be83c3c7a8008b8fe46a
Евгений Остапенко <ostapenko.evgenii@otr.ru>
2021-09-08 22:18:19 +0300

    fix: removed unused code

--- src/main/java/org/taurus/aya/server/services/JiraService.java
+++ src/main/java/org/taurus/aya/server/services/JiraService.java
@@ -1,12 +1,21 @@
 package org.taurus.aya.server.services;
 
+import io.netty.handler.logging.LogLevel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.taurus.aya.client.EventState;
 import org.taurus.aya.server.UserRepository;
 import org.taurus.aya.server.entity.User;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +24,14 @@ import java.util.Map;
 @Slf4j
 public class JiraService {
 
-	private String JIRA_URL = "https://job-jira.otr.ru";
+	@Value("${aya.jira.url}")
+	private String JIRA_URL;
+	@Value("${aya.jira.start_code}")
+	private String JIRA_START_CODE;
+	@Value("${aya.jira.pause_code}")
+	private String JIRA_PAUSE_CODE;
+	@Value("${aya.jira.stop_code}")
+	private String JIRA_STOP_CODE;
 	private String JIRA_REST_ISSUE = "/rest/api/2/issue/";
 
 	@Autowired
@@ -25,10 +41,42 @@ public class JiraService {
 		public TransitionEntity(){
 			transition = new HashMap<>();
 		}
+
+		public Map<String, String> getTransition() {
+			return transition;
+		}
+
+		public void setTransition(Map<String, String> transition) {
+			this.transition = transition;
+		}
+
 		private Map<String,String> transition;
 	}
 
-	private WebClient client = WebClient.create(JIRA_URL);
+	private WebClient client = WebClient.builder()
+			.clientConnector(new ReactorClientHttpConnector(
+					HttpClient.create().wiretap(true)
+			))
+			.baseUrl(JIRA_URL)
+			.filters(exchangeFilterFunctions -> {
+				exchangeFilterFunctions.add(logRequest());
+				exchangeFilterFunctions.add(logResponse());
+			})
+			.build();
+		//WebClient.create(JIRA_URL);
+
+	public JiraService(){
+		client = WebClient.builder()
+				.clientConnector(new ReactorClientHttpConnector(
+						HttpClient.create().wiretap(true)
+				))
+				.filters(exchangeFilterFunctions -> {
+					exchangeFilterFunctions.add(logRequest());
+					exchangeFilterFunctions.add(logResponse());
+				})
+				.baseUrl(JIRA_URL)
+				.build();
+	}
 
 	public void performTransition(String jiraTaskId, Long userId, Integer state){
 		User user = userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
@@ -37,18 +85,53 @@ public class JiraService {
 
 	/**Конвертация кода задачи Aya в код задачи JIRA (JIRA хочет String)*/
 	private String convertState(Integer state){
-		return "1";
+		switch (state){
+			case 1: return JIRA_START_CODE;
+			case 2:
+			case 3: return JIRA_PAUSE_CODE;
+			default: return JIRA_PAUSE_CODE;
+		}
 	}
 
 	private void doRest(String task, String stateCode, String login, String password){
+		System.out.println("JIRA_URL = " + JIRA_URL);
 		TransitionEntity request = new TransitionEntity();
 		request.transition.put("id", stateCode);
-		WebClient.ResponseSpec responseSpec = client.post()
-				.uri(JIRA_REST_ISSUE)
-				.bodyValue(request)
+		Object obj = client.post()
+				.uri(JIRA_URL + JIRA_REST_ISSUE + task + "/transitions")
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(Mono.just(request),TransitionEntity.class)
 				.headers(headers -> headers.setBasicAuth(login, password))
-				.retrieve();
-		System.out.println("responseSpec = " + responseSpec);
+				.retrieve()
+				.onStatus(HttpStatus::is4xxClientError,
+				         error -> Mono.error(new RuntimeException("API not found"))).bodyToMono(Void.class).block();
+		System.out.println("responseSpec = " + obj);
+	}
+
+	ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+			if (log.isDebugEnabled()) {
+				StringBuilder sb = new StringBuilder("Request: \n");
+				//append clientRequest method and url
+				clientRequest
+						.headers()
+						.forEach((name, values) -> values.forEach(value -> sb.append("header: ").append(value)));
+				log.debug(sb.toString());
+			}
+			return Mono.just(clientRequest);
+		});
+	}
+
+	ExchangeFilterFunction logResponse() {
+		return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+			if (log.isDebugEnabled()) {
+				StringBuilder sb = new StringBuilder("Request: \n");
+				//append clientRequest method and url
+				sb.append("code: ").append(clientResponse.rawStatusCode());
+				log.debug(sb.toString());
+			}
+			return Mono.just(clientResponse);
+		});
 	}
 
 }
